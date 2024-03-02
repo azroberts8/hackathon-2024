8815ac3db4077d81f8c707aa17a78684e6cf8986
Yasmeen <yelzamek@udel.edu>
2024-03-02 18:24:18 -0500

    refactoring nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,13 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Search } from "../components/Search.tsx";
+
 
 const NavigationBar = () => {
 return (<div class="py-2 px-2 bg-[#86efac]" >
-      <div class="max-w-screen-md flex flex-col">
-       
-
+      <div class="max-w-screen-md flex flex-row">
         <a href="/"><h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1></a>
-  
-       
-
+        <Search></Search>
       </div>
     </div>);
 
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,6 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 import { Button } from "../components/Button.tsx";
-import { Search } from "../components/Search.tsx";
 import NavigationBar from "../components/Nav.tsx";
 
 
@@ -11,10 +10,5 @@ export default function Home() {
    <div>
     <NavigationBar />
    </div>
-    <div class="py-2 px-2 bg-[#86efac]" >
-      <h1 class="text-4xl font-bold align-middle flex flex-row ">Quants Made Simple
-      <Search class=""></Search>
-      </h1> 
-    </div>
   );
 }
