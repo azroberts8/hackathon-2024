067bcc2700d41f3a4f5b706ae09a9c4b451c2488
Colby <cdolbow@udel.edu>
2024-03-02 16:42:03 -0500

    Aligned Nav bar text to left

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const count = useSignal(3);
   return (
     <div class="bg-[#86efac]" >
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+      <div class="max-w-screen-md flex flex-col">
         <h1 class="text-4xl font-bold">Quants Made Simple</h1>
         <p class="my-4">
         </p>
