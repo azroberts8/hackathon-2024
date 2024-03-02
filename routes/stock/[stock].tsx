1dd31944b679ae6434c460767e91a507f01c316b
mattgwin29 <mattgwin@udel.edu>
2024-03-02 17:46:16 -0500

    modify order (query by stock abbreviation)

--- routes/stock/[stock].tsx
+++ routes/stock/[stock].tsx
@@ -6,7 +6,7 @@ export default function Stock(props: PageProps) {
     <div class="px-4 py-8 mx-auto bg-[#FFFFFF]">
       <div class=" mx-auto flex flex-col ">
       {/* <StockBio s ={"Example", "ex", "This is an example stock"}/> */}
-        {StockBio(props.params.stock, "Abbreviation", "Here is an example Description")}
+        {StockBio(props.params.stock, "Full Name", "Here is an example Description")}
       </div>
     </div>
   ); 
