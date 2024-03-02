b5dae94bffebd5de2b4c85c17bb7b6bec5595d4c
Colby <cdolbow@udel.edu>
2024-03-02 18:08:28 -0500

    Navigation Bar componet

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,14 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import { Button } from "../components/Button.tsx"
+import NavigationBar from "../components/Nav.tsx";
+
 
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="py-2 px-2 bg-[#86efac]" >
-      <div class="max-w-screen-md flex flex-col">
-        <h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1>
-        
-      </div>
-    </div>
+   <div>
+    <NavigationBar />
+   </div>
   );
 }
