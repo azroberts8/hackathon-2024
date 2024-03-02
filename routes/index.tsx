9b768ee26a3194a2e099f0f38ed84f00475c7556
Colby <cdolbow@udel.edu>
2024-03-02 16:48:21 -0500

    Further algined text

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,11 +4,10 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="bg-[#86efac]" >
+    <div class="py-2 px-2 bg-[#86efac]" >
       <div class="max-w-screen-md flex flex-col">
-        <h1 class="text-4xl font-bold">Quants Made Simple</h1>
-        <p class="my-4">
-        </p>
+        <h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1>
+        
       </div>
     </div>
   );
