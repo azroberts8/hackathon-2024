b75be8b59d2dc3ea59cc741d5b0c518a4610761f
Yasmeen <yelzamek@udel.edu>
2024-03-02 18:16:19 -0500

    search barrrr

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,16 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import { Button } from "../components/Button.tsx";
+import { Search } from "../components/Search.tsx";
+
 
 export default function Home() {
   const count = useSignal(3);
   return (
     <div class="py-2 px-2 bg-[#86efac]" >
-      <div class="max-w-screen-md flex flex-col">
-        <h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1>
-        
-      </div>
+      <h1 class="text-4xl font-bold align-middle flex flex-row ">Quants Made Simple
+      <Search class=""></Search>
+      </h1> 
     </div>
   );
 }
