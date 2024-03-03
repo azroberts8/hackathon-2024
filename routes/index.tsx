8cfc8823d3e20efe1f6e7b1e63286b4bd30f66fc
mattgwin29 <mattgwin@udel.edu>
2024-03-03 04:01:55 -0500

    Add forms to search bar

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,6 @@ import Counter from "../islands/Counter.tsx";
 import { Button } from "../components/Button.tsx";
 import NavigationBar from "../components/NavigationBar.tsx";
 
-
 export default function Home() {
   const count = useSignal(3);
   return (
--- routes/stock/[stock].tsx
+++ routes/stock/[stock].tsx
@@ -12,6 +12,21 @@ export const handler: Handlers<number[]> = {
     const data = await response;
     return ctx.render(data);
   },
+  async POST(req, ctx) {
+    const form = await req.formData();
+    const email = form.get("text")?.toString();
+    console.log("AHHHHHHHHHHH");
+    // Add email to list.
+
+    // Redirect user to thank you page.
+    const headers = new Headers();
+    headers.set("location", "/thanks-for-subscribing");
+    return new Response(null, {
+      status: 303, // See Other
+      headers,
+    });
+  }
+
 };
 
 export default function Stock(props: PageProps) {
