6896c9fda41c1afb487328ae510f4029e95a3bd3
Yasmeen <yelzamek@udel.edu>
2024-03-02 19:39:08 -0500

    font change

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,7 +6,7 @@ import { Search } from "../components/Search.tsx";
 const NavigationBar = () => {
 return (<div class="py-2 px-2 bg-[#86efac]" >
       <div class="max-w-screen-md flex flex-row">
-        <a href="/"><h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1></a>
+        <a href="/"><h1 class="text-4xl font-bold font-mono inline-block align-middle">Quants Made Simple</h1></a>
         <div class=""><Search></Search></div>
       </div>
     </div>);
