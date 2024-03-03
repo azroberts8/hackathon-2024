1899a06bfca168bb25844f0f5b4908ce20c8f125
Yasmeen <yelzamek@udel.edu>
2024-03-03 04:38:21 -0500

    card-like items w buy and sell buttons

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -12,17 +12,32 @@ export interface ListItemInterface {
 export default function ListItem({ ticker, name, price, sharesCount }: ListItemInterface) {
     return (
         <div>
-            <div class="px-5 py-5 flex justify-between items-center h-10 overflow-hidden">
-                <a href = {"/stock/" + ticker}>
-                <button class = "h-20 w-60 px-2 py-1 border-gray-500 border-2 bg-white hover:bg-gray-200 transition-colors">
-                    <h1 class = "text-left">{ticker}</h1>
-                    {name} {price} {sharesCount} 
-                </button>
-                </a>
+            <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                </div>
+                <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                        <div class="text-gray-900 font-bold text-xl mb-2">{ticker}</div>
+                        <p class="text-green-700 text-base">${price}</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-start">
+                    <button class="bg-green-500 text-white px-4 py-2 rounded-md">
+                        Buy
+                    </button>
+
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md mt-2">
+                        Sell
+                    </button>
+                </div>
+
             </div>
+
+
         </div>
 
-        
+
     );
 }
 
