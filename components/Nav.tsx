d9ac72a19a99ebd7b14ead99cf1e7f2afacc1ec3
Andrew Roberts <andrew@azroberts.com>
2024-03-02 19:53:58 -0500

    Changed navbar to red

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ import { Search } from "../components/Search.tsx";
 
 
 const NavigationBar = () => {
-return (<div class="py-2 px-2 bg-[#86efac]" >
+return (<div class="py-2 px-2 bg-[#FF0000]" >
       <div class="max-w-screen-md flex flex-row">
         <a href="/"><h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1></a>
         <Search></Search>
