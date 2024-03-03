ad6353b993d9c8d7ea8f8ec1fd4d4c421242e1c5
Yasmeen <yelzamek@udel.edu>
2024-03-02 20:01:48 -0500

    profile and nav bar changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,10 +4,11 @@ import { Search } from "../components/Search.tsx";
 
 
 const NavigationBar = () => {
-return (<div class="py-2 px-2 bg-[#86efac]" >
+return (<div class="py-2 px-2 bg-[#86efac] " >
       <div class="max-w-screen-md flex flex-row">
-        <a href="/"><h1 class="text-4xl font-bold font-mono inline-block align-middle">Quants Made Simple</h1></a>
-        <div class=""><Search></Search></div>
+        <a href="/"><h1 class="text-4xl font-bold font-mono inline-block align-middle">quants_made_simple</h1></a>
+        <div class="px-80"><Search></Search></div>
+        <a href="/profile"><h1 class="px-80 text-2xl font-bold font-mono inline-block align-middle">my_profile</h1></a>
       </div>
     </div>);
 
