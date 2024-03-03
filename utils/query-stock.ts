467d481f06daf16204ec1aadec84c249c699b408
Andrew Roberts <andrew@azroberts.com>
2024-03-03 03:51:11 -0500

    Reformatted deep stock query

--- utils/query-stock.ts
+++ utils/query-stock.ts
@@ -6,11 +6,24 @@ interface StockQueryParams {
 }
 
 interface BasicStockResponse {
-
+  symbol: string;
+  price: number;
+  beta: number;
+  changes: number;
+  companyName: string;
+  industry: string;
+  sector: string;
+  description: string;
+  image: string;
 }
 
-interface DeepStockResponse {
-
+interface DeepStockResponse extends BasicStockResponse {
+  revenuePerShare: number;
+  peRatio: number;
+  priceToSalesRatio: number;
+  debtToEquity: number;
+  freeCashFlowPerShare: number;
+  payoutRatio: number;
 }
 
 const POLYGON_API_KEY = Deno.env.get("POLYGON_API_KEY");
@@ -25,16 +38,29 @@ const StockPriceQuery = async ({ ticker, from = new Date("2024-03-01"), to = new
   return polyResponse.results.map((x: { c: number }) => x.c);
 }
 
-const StockBasicQuery = async ({ ticker }: StockQueryParams): Promise<any> => {
+const StockBasicQuery = async ({ ticker }: StockQueryParams): Promise<BasicStockResponse> => {
   const financialResponse = await fetch(`https://financialmodelingprep.com/api/v3/profile/${ ticker }?apikey=${ FINANCIAL_MODELING_API_KEY }`)
     .then(res => res.json())
     .then(res => res[0]);
+  return financialResponse;
 }
 
-const StockDeepQuery = async ({ ticker }: StockQueryParams): Promise<any> => {
-  const financialResponse = await fetch(`https://financialmodelingprep.com/api/v3/key-metrics/${ ticker }?period=annual&apikey=${ FINANCIAL_MODELING_API_KEY }`)
-    .then(res => res.json())
-    .then(res => res[0]);
+const StockDeepQuery = async ({ ticker }: StockQueryParams): Promise<DeepStockResponse> => {
+  const [basic, deep] = await Promise.all([
+    StockBasicQuery({ ticker }),
+    fetch(`https://financialmodelingprep.com/api/v3/key-metrics/${ ticker }?period=annual&apikey=${ FINANCIAL_MODELING_API_KEY }`)
+      .then(res => res.json())
+      .then(res => res[0])
+  ])
+  return {
+    ...basic,
+    revenuePerShare: deep.revenuePerShare,
+    peRatio: deep.peRatio,
+    priceToSalesRatio: deep.priceToSalesRatio,
+    debtToEquity: deep.debtToEquity,
+    freeCashFlowPerShare: deep.freeCashFlowPerShare,
+    payoutRatio: deep.payoutRatio
+  }
 }
 
 export { StockPriceQuery, StockBasicQuery, StockDeepQuery }
