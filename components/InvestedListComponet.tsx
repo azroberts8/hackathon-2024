093453342aad2310b87c802a2eecf593e9e58634
Colby <cdolbow@udel.edu>
2024-03-02 23:01:29 -0500

    Invested componet list

--- components/InvestedListComponet.tsx
+++ components/InvestedListComponet.tsx
@@ -1,11 +1,11 @@
 export default function InvestedListComponet(ticker: string, shares: number, price: number) {
 return (
 <div class="border border-b-2 flex flex-row"> 
-<div class="object-left">
+<div class="object-left px-1">
   <p class="text-base" >{ticker}</p>
   <p class="text-sm ">{shares}</p>
-  </div><div class="border border-solid">
-  <p class="text-xs text-right align-middle grid-cols-2 border border-solid">{price}</p>
+  </div><div class="border border-solid object-right">
+  <p class="py-4 px-1 text-xs text-right align-middle grid-cols-2 border border-solid">{price}</p>
  </div>
 
 
