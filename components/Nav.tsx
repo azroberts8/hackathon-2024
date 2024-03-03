a25fb0d477a5481439ca1d2422105810666bd300
Yasmeen <yelzamek@udel.edu>
2024-03-02 19:35:31 -0500

    search bar changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ const NavigationBar = () => {
 return (<div class="py-2 px-2 bg-[#86efac]" >
       <div class="max-w-screen-md flex flex-row">
         <a href="/"><h1 class="text-4xl font-bold inline-block align-middle">Quants Made Simple</h1></a>
-        <Search></Search>
+        <div class=""><Search></Search></div>
       </div>
     </div>);
 
