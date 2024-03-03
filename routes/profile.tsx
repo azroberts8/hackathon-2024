f309b6caea7da1246b4170d196592cd85d3d4f45
Yasmeen <yelzamek@udel.edu>
2024-03-02 21:51:23 -0500

    add nav bar to profile page

--- routes/profile.tsx
+++ routes/profile.tsx
@@ -1,8 +1,13 @@
 import { PageProps } from "$fresh/server.ts";
+import NavigationBar from "../components/Nav.tsx";
+
 
 export default function Profile(props: PageProps) {
     return (
-      <div class="px-4 py-8 mx-auto bg-[#FF31EC]"> MY PROFILE BABYYY
-      </div>
-    ); 
-  }
+        <div>
+            <NavigationBar />
+            <div class="px-4 py-8 mx-auto bg-[#FF31EC]"> MY PROFILE BABYYY</div>
+        </div>
+
+    );
+}
