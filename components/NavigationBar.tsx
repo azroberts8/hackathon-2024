d54d31c57f039c64ee720652b6358ddec63125d2
Andrew Roberts <andrew@azroberts.com>
2024-03-02 20:48:44 -0500

    Fixed navbar layout

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,43 @@
+import { JSX } from "preact";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Search } from "./Search.tsx";
+
+
+const NavigationBar = () => {
+  return(
+    <div class="py-2 px-2 bg-[#86efac]">
+      <div class="flex w-full justify-between items-center">
+        <div class="">
+          <a href="/"><h1 class="text-4xl font-bold font-mono inline-block align-middle">quants_made_simple</h1></a>
+        </div>
+        <div class="">
+          <div class="inline-block mr-8">
+            <Search />
+          </div>
+          <div class="inline-block">
+            <a href="/profile">
+            <h1 class="text-2xl font-bold font-mono inline-block align-middle">my_profile</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+
+// return (<div class="py-2 px-2 bg-[#86efac] " >
+//       <div class="max-w-screen-md flex flex-row">
+//         <a href="/">
+//           <h1 class="text-4xl font-bold font-mono inline-block align-middle">quants_made_simple</h1>
+//         </a>
+//         <div class="float-end">
+//           <Search />
+//         </div>
+//         <a href="/profile">
+//           <h1 class="float-end text-2xl font-bold font-mono inline-block align-middle">my_profile</h1>
+//         </a>
+//       </div>
+//     </div>);
+
+}
+export default NavigationBar;
